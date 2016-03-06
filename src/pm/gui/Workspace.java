e3ff10acc3bcb0dd169bd7b0377003e0e821741a
哲 <@10.245.244.185>
2016-03-05 20:38:35 -0500

    Uploaded 1. Initialized the GUI.    Buttons, labels and a slider are added.

--- src/pm/gui/Workspace.java
+++ src/pm/gui/Workspace.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -19,6 +21,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import pm.PoseMaker;
 import pm.controller.PageEditController;
@@ -31,6 +34,8 @@ import saf.ui.AppYesNoCancelDialogSingleton;
 import pm.file.FileManager;
 import pm.data.DataManager;
 import pm.controller.PageEditController;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * This class serves as the workspace component for this application, providing
@@ -46,6 +51,8 @@ public class Workspace extends AppWorkspaceComponent {
     static final String CLASS_MAX_PANE = "max_pane";
     static final String CLASS_RENDER_CANVAS = "render_canvas";
     static final String CLASS_BORDER_PANE = "bordered_pane";
+    static final String CLASS_EDIT_TOOLBAR = "edit_toolbar";
+    static final String CLASS_SUBHEADING_LABEL = "subheading_label";
     
     
     // HERE'S THE APP
@@ -63,6 +70,16 @@ public class Workspace extends AppWorkspaceComponent {
     // THESE ARE THE BUTTONS FOR ADDING AND REMOVING COMPONENTS
     BorderPane leftPane;
     BorderPane tagToolbar;
+    BorderPane ButtonPane;
+    BorderPane ColorPane;
+    BorderPane BottomTool;
+    FlowPane ShapeButtons;
+    FlowPane FrontBack;
+    VBox BackGroundColor;
+    VBox FillColor;
+    VBox OutlineColor;
+    VBox Thickness;
+    FlowPane Snapshot;
     
     // THE CANVAS
     Pane rightPane;
@@ -71,8 +88,26 @@ public class Workspace extends AppWorkspaceComponent {
     AppMessageDialogSingleton messageDialog;
     AppYesNoCancelDialogSingleton yesNoCancelDialog;
 
-    Button aButton;
-    Button bButton;
+    // THE BUTTONS
+    Button selectButton;
+    Button removeButton;
+    Button rectButton;
+    Button ellipseButton;
+    Button frontButton;
+    Button backButton;
+    Button bgcButton;
+    Button fcButton;
+    Button otcButton;
+    Button SnapshotButton;
+    
+    // LABELS
+    Label bgColor;
+    Label fColor;
+    Label otColor;
+    Label otThick;
+    
+    // SLIDER
+    Slider outlineSlider;
     
     /**
      * Constructor for initializing the workspace, note that this constructor
@@ -109,43 +144,140 @@ public class Workspace extends AppWorkspaceComponent {
 
 	// THIS IS THE TOP TOOLBAR
 	tagToolbar = new BorderPane();
-	//tagToolbarScrollPane = new ScrollPane(tagToolbar);
-	//tagToolbarScrollPane.setFitToHeight(true);
+        ButtonPane = new BorderPane();
+        ColorPane = new BorderPane();
+        BottomTool = new BorderPane();
+        ShapeButtons = new FlowPane();
+        FrontBack = new FlowPane();
+        BackGroundColor = new VBox();
+        FillColor = new VBox();
+        OutlineColor = new VBox();
+        Thickness = new VBox();
+        Snapshot = new FlowPane();
 
 
 	// LOAD ALL SHAPES
 	FileManager fileManager = (FileManager) app.getFileComponent();
 	DataManager dataManager = (DataManager) app.getDataComponent();
 
-        aButton = new Button("X");
-	aButton.setMaxWidth(75);
-	aButton.setMinWidth(75);
-	aButton.setPrefWidth(75);
-	tagToolbar.getChildren().add(aButton);
+        tagToolbar.setMaxWidth(275);
+        tagToolbar.setMaxHeight(800);
         
-        bButton = new Button("X");
-	bButton.setMaxWidth(75);
-	bButton.setMinWidth(75);
-	bButton.setPrefWidth(75);
-	//rightPane.getChildren().add(bButton);
+        // INITIAL THE STYLE OF THE TOP TWO PANES
+        ShapeButtons.setHgap(15);
+        ShapeButtons.setPadding(new Insets(15));
         
+        FrontBack.setHgap(15);
+        FrontBack.setPadding(new Insets(15));
+        
+        // THE BUTTONS OF THE TOP TWO PANES
+        selectButton = new Button();
+        Image selectImage = new Image("file:./images/SelectionTool.png");
+        selectButton.setGraphic(new ImageView(selectImage));
+	ShapeButtons.getChildren().add(selectButton);
+   
+        removeButton = new Button();
+        Image removeImage = new Image("file:./images/Remove.png");
+        removeButton.setGraphic(new ImageView(removeImage));
+	ShapeButtons.getChildren().add(removeButton);
+        
+        rectButton = new Button();
+        Image rectImage = new Image("file:./images/Rect.png");
+        rectButton.setGraphic(new ImageView(rectImage));
+	ShapeButtons.getChildren().add(rectButton);
+        
+        ellipseButton = new Button();
+        Image ellipseImage = new Image("file:./images/Ellipse.png");
+        ellipseButton.setGraphic(new ImageView(ellipseImage));
+	ShapeButtons.getChildren().add(ellipseButton);
+        
+        frontButton = new Button();
+        Image frontImage = new Image("file:./images/MoveToFront.png");
+        frontButton.setGraphic(new ImageView(frontImage));
+	FrontBack.getChildren().add(frontButton);
+        
+        backButton = new Button();
+        Image backImage = new Image("file:./images/MoveToBack.png");
+        backButton.setGraphic(new ImageView(backImage));
+	FrontBack.getChildren().add(backButton);
+        
+        // FOR THE BUTTONS SPACES
+        ButtonPane.setTop(ShapeButtons);
+        ButtonPane.setBottom(FrontBack);
+        
+        // BACKGROUND COLOR
+        BackGroundColor.setPadding(new Insets(15));
+        BackGroundColor.setSpacing(15);
+        
+        bgColor = new Label("Background Color");
+        BackGroundColor.getChildren().add(bgColor);
+        
+        bgcButton = new Button("background color");
+        BackGroundColor.getChildren().add(bgcButton);
+        
+        // FILL COLOR
+        FillColor.setPadding(new Insets(15));
+        FillColor.setSpacing(15);
+        
+        fColor = new Label("Fill Color");
+        FillColor.getChildren().add(fColor);
+        
+        fcButton = new Button("fill color");
+        FillColor.getChildren().add(fcButton);
+        
+        // OUTLINE COLOR
+        OutlineColor.setPadding(new Insets(15));
+        OutlineColor.setSpacing(15);
+        
+        otColor = new Label("Outline Color");
+        OutlineColor.getChildren().add(otColor);
+        
+        otcButton = new Button("outline color");
+        OutlineColor.getChildren().add(otcButton);
+        
+        // FOR COLOR SPACES
+        ColorPane.setTop(BackGroundColor);
+        ColorPane.setCenter(FillColor);
+        ColorPane.setBottom(OutlineColor);
 
-	// AND NOW THE REGION FOR EDITING TAG PROPERTIES
-	//tagEditorPane = new GridPane();
-	//tagEditorScrollPane = new ScrollPane(tagEditorPane);
-
+        // FOR THICKNESS TOOL
+        Thickness.setPadding(new Insets(15));
+        Thickness.setSpacing(15);
+        
+        otThick = new Label("Outline Thickness");
+        Thickness.getChildren().add(otThick);
+        
+        outlineSlider = new Slider();
+        Thickness.getChildren().add(outlineSlider);
+        
+        // FOR SNAP SHOT
+        Snapshot.setPadding(new Insets(15));
+        
+        
+        SnapshotButton = new Button();
+        Image ssImage = new Image("file:./images/Snapshot.png");
+        SnapshotButton.setGraphic(new ImageView(ssImage));
+	Snapshot.getChildren().add(SnapshotButton);
+        
+        
+        // FOR BOTTOM 2 TOOLS
+        BottomTool.setTop(Thickness);
+        BottomTool.setCenter(Snapshot);
+        
+        // SET UP THE SPACES FOR TOOLS
+        tagToolbar.setTop(ButtonPane);
+        tagToolbar.setCenter(ColorPane);
+        tagToolbar.setBottom(BottomTool);
 
 	// PUT THEM IN THE LEFT
-	leftPane.setCenter(tagToolbar);
-	//leftPane.setBottom(tagEditorScrollPane);
+	leftPane.setTop(tagToolbar);
+
         
         // AND NOW PUT IT IN THE WORKSPACE
 	poseWorkSpace.setLeft(leftPane);
 	poseWorkSpace.setCenter(rightPane);
 
 	// AND FINALLY, LET'S MAKE THE SPLIT PANE THE WORKSPACE
-        //workspace.setLeft(leftPane);
-        //workspace.setRight(rightPane);
         workspace.setCenter(poseWorkSpace);
 
         // NOTE THAT WE HAVE NOT PUT THE WORKSPACE INTO THE WINDOW,
@@ -166,11 +298,19 @@ public class Workspace extends AppWorkspaceComponent {
 	// NOTE THAT EACH CLASS SHOULD CORRESPOND TO
 	// A STYLE CLASS SPECIFIED IN THIS APPLICATION'S
 	// CSS FILE
-        //tagToolbar.getStyleClass().add(CLASS_MAX_PANE);
-        leftPane.getStyleClass().add(CLASS_BORDER_PANE);
         rightPane.getStyleClass().add(CLASS_RENDER_CANVAS);
-        //poseWorkSpace.getStyleClass().add(CLASS_BORDER_PANE);
-    
+        leftPane.getStyleClass().add(CLASS_BORDER_PANE);
+        ShapeButtons.getStyleClass().add(CLASS_MAX_PANE);
+        FrontBack.getStyleClass().add(CLASS_MAX_PANE);
+        BackGroundColor.getStyleClass().add(CLASS_MAX_PANE);
+        FillColor.getStyleClass().add(CLASS_MAX_PANE);
+        OutlineColor.getStyleClass().add(CLASS_MAX_PANE);
+        bgColor.getStyleClass().add(CLASS_SUBHEADING_LABEL);
+        fColor.getStyleClass().add(CLASS_SUBHEADING_LABEL);
+        otColor.getStyleClass().add(CLASS_SUBHEADING_LABEL);
+        Thickness.getStyleClass().add(CLASS_MAX_PANE);
+        otThick.getStyleClass().add(CLASS_SUBHEADING_LABEL);
+        Snapshot.getStyleClass().add(CLASS_MAX_PANE);
     }
 
     /**
